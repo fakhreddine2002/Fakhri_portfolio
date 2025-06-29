451a73a7fa3463ccc626756e35e4826a27321393
fakhreddine2002 <fakhrieddine.mahfoudh@ensi-uma.tn>
2025-06-29 01:02:19 +0100

    jjlklkkj

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -24,8 +24,9 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
                         Contact
                     </a>
                    <a
-                        href="https://raw.githubusercontent.com/Fakhreddine2002/Fakhri_portfolio/main/public/_cv_.pdf"
-                        download
+                        href="/Fakhri_portfolio/_cv_.pdf"
+                        target="_blank"
+                        rel="noopener noreferrer"
                         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-300"
                     >
                         Download CV
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,8 +43,9 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
                     </a>
 
                     <a
-                        href="https://raw.githubusercontent.com/Fakhreddine2002/Fakhri_portfolio/main/public/_cv_.pdf"
-                        download
+                        href="/Fakhri_portfolio/_cv_.pdf"
+                        target="_blank"
+                        rel="noopener noreferrer"
                         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-300"
                     >
                         Download CV
