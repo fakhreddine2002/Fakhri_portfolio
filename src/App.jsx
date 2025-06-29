4e3bcc956717733c3863388758aead17cd1146a3
fakhreddine2002 <fakhrieddine.mahfoudh@ensi-uma.tn>
2025-06-30 00:42:21 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
       <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-gray-950 text-gray-100 `}>
         < Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         < MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <br />
         <Home />
         <About />
         <Projects />
