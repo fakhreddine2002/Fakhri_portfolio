7d98001dce80c75820c887ee5890752c3bb9bf7b
fakhreddine2002 <fakhrieddine.mahfoudh@ensi-uma.tn>
2025-06-29 15:24:16 +0100

    .

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -25,7 +25,6 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
                     </a>
                     <a
                         href="/Fakhri_portfolio/_cv_.pdf"
-                        download="Fakhri_CV.pdf"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-300"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,6 @@ export const Navbar = ({ menuOpen, setMenuOpen }) => {
 
                     <a
                         href="/Fakhri_portfolio/_cv_.pdf"
-                        download="Fakhri_CV.pdf"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-300"
